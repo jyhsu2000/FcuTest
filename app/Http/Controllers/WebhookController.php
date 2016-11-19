0ab9b87917239256684d2f653dab01a80324b4d5
KID <jyhsu2000@yahoo.com.tw>
2016-11-20 00:41:34 +0800

    Extract handleMessage

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -21,35 +21,52 @@ class WebhookController extends Controller
         if (!isset($json['entry'][0]['messaging'][0])) {
             return response()->json([]);
         }
-        $message = '';
-        if (isset($json['entry'][0]['messaging'][0]['text'])) {
-            $message = 'Echo: ' . $json['entry'][0]['messaging'][0]['text'];
-        } elseif (isset($json['entry'][0]['messaging'][0]['attachments'])) {
-            $message = 'url: ' . $json['entry'][0]['messaging'][0]['attachments'][0]['payload']['url'];
+
+        //逐一處理每一則訊息
+        $messageJSONs = $json['entry'][0]['messaging'];
+        foreach ($messageJSONs as $messageJSON) {
+            $this->handleMessage($messageJSON);
         }
 
-        $recipientID = isset($json['entry'][0]['messaging'][0]['sender']['id'])
-            ? $json['entry'][0]['messaging'][0]['sender']['id'] : null;
-        if (!$recipientID) {
-            return response()->json(['No recipientID']);
+        return response()->json();
+    }
+
+    private function handleMessage($messageJSON)
+    {
+        //設定回覆訊息
+        $responseMessage = '';
+        if (isset($messageJSON['text'])) {
+            $responseMessage = 'Echo: ' . $messageJSON['text'];
+        } elseif (isset($messageJSON['attachments'])) {
+            $responseMessage = 'url: ' . $messageJSON['attachments'][0]['payload']['url'];
         }
+
+        //回覆對象
+        $recipientID = isset($messageJSON['sender']['id']) ? $messageJSON['sender']['id'] : null;
+        if (!$recipientID) {
+            return false;
+        }
+
+        \Log::info('RecipientID: ', $recipientID);
 
         //送出訊息
         $client = new Client(['base_uri' => 'https://graph.facebook.com/v2.6/me/messages']);
         $response = $client->post('/', [
-            'query' => ['access_token' => env('FB_BOT_TOKEN')],
+            'query' => [
+                'access_token' => env('FB_BOT_TOKEN'),
+            ],
             'json'  => [
                 'recipient' => [
                     'id' => $recipientID,
                 ],
                 'message'   => [
-                    'text' => $message,
+                    'text' => $responseMessage,
                 ],
             ],
         ]);
 
         \Log::info('Response: ', $response);
 
-        return response()->json();
+        return true;
     }
 }
